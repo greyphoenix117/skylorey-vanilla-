46c14fd59e818630948aed1a6f09f9db348c9afe
greyphoenix117 <46097455+greyphoenix117@users.noreply.github.com>
2023-06-27 16:20:44 -0700

    updated algo to reylo
    Updated the default algo file to reylo. Added .env file with data for publishing the custom feed.

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -8,24 +8,24 @@ const run = async () => {
 
   // YOUR bluesky handle
   // Ex: user.bsky.social
-  const handle = ''
+  const handle = process.env.bsky_handle ?? ''
 
   // YOUR bluesky password, or preferably an App Password (found in your client settings)
   // Ex: abcd-1234-efgh-5678
-  const password = ''
+  const password = process.env.bsky_app_password ?? ''
 
   // A short name for the record that will show in urls
   // Lowercase with no spaces.
   // Ex: whats-hot
-  const recordName = ''
+  const recordName = process.env.record_name ?? ''
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = ''
+  const displayName = process.env.display_name ?? ''
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
-  const description = ''
+  const description = process.env.feed_desc ?? ''
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
